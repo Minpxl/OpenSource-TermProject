d45534693c5965d502789334a0db8a20e014a110
이수민 <tnals_0618@naver.com>
2022-12-13 20:30:36 +0900

    fix2

--- sharpening.py
+++ sharpening.py
@@ -1,5 +1,5 @@
 import cv2
-import math
+import numpy as np
 
 src=cv2.imread('c:/image/road.jpg')
 if src is None:
@@ -7,9 +7,10 @@ if src is None:
     sys.exit()
 
 gray=cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-canny = cv2.Canny(src, 100, 255)
+sharp = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
 
+sharpening = cv2.filter2D(gray, -1, sharp)
 
-cv2.imshow("canny",canny)
+cv2.imshow('sharpening2', sharpening)
 cv2.waitKey()
 cv2.destroyAllWindows()
